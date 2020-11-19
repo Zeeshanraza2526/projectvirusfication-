b0eb0896c84d9430a463f2a8dff241f99e09a1d4
Zeeshanraza2526 <74452961+Zeeshanraza2526@users.noreply.github.com>
2020-11-19 11:35:46 +0500

    Add files via upload

---
+++ css/index.php
@@ -0,0 +1,22 @@
+<?php
+include('includes/header.php'); 
+include('includes/navbar.php'); 
+?>
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+
+
+
+
+
+
+
+
+
+  <?php
+include('includes/scripts.php');
+include('includes/footer.php');
+?>
