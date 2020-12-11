9adbc56d1a5179b91c650dd3e08b43fd627a119a
Zeeshanraza2526 <74452961+Zeeshanraza2526@users.noreply.github.com>
2020-12-11 17:16:20 +0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -56,7 +56,7 @@ if (!isset($_SESSION['username'])) {
                            <li><a href=".html">Symptoms Checker</a></li>
                            <li><a href="https://www.facebook.com/IntelBot-108875797526008">IntelBot</a></li>
                            <li><a href="videos.html">Videos</a></li>
-                           <li><a href="pictures.html">Pictures</a></li>
+                           <li><a href="pictures.html"></a></li>
                            
                        </ul>
                    </li>
